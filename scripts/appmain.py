27d0cb7b2e9fbaa9aaa8ad4c1f4343da7adf8558
Gustavo Oliveira <gutti.bp@gmail.com>
2024-11-15 16:41:16 -0300

    Update appmain.py

--- scripts/appmain.py
+++ scripts/appmain.py
@@ -1,13 +1,16 @@
-  # Certifique-se de que `sobreoprojeto.py` está no mesmo diretório
+# Certifique-se de que `sobreoprojeto.py`, `home.py`, e `sobreonegocio.py` estão no mesmo diretório
 import streamlit as st
 import home
 import sobreonegocio
 import sobreoprojeto
 
-
-# Menu lateral
+# Menu lateral com a página "Home" selecionada como padrão
 st.sidebar.title("Menu")
-menu = st.sidebar.radio("Selecione uma página:", ["Home", "Sobre o Negócio", "Sobre o Projeto"])
+menu = st.sidebar.radio(
+    "Selecione uma página:", 
+    ["Home", "Sobre o Negócio", "Sobre o Projeto"], 
+    index=0  # Define "Home" como padrão (índice 0)
+)
 
 # Exibição das páginas
 if menu == "Home":

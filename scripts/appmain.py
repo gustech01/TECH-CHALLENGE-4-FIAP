6fe67e7105dc122b76ddf550af9282007ee0a325
Gustavo Oliveira <gutti.bp@gmail.com>
2024-11-15 15:47:00 -0300

    Update appmain.py

--- scripts/appmain.py
+++ scripts/appmain.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import sobreoprojeto  # Certifique-se de que `sobreoprojeto.py` está no mesmo diretório
-
+import sobreonegocio
+import home
 # Menu lateral
 st.sidebar.title("Menu")
 menu = st.sidebar.radio("Selecione uma página:", ["Home", "Sobre o Negócio", "Sobre o Projeto"])
@@ -11,3 +12,20 @@ if menu == "Sobre o Projeto":
         sobreoprojeto.show()
     except AttributeError:
         st.error("Erro ao carregar a página 'Sobre o Projeto'. Verifique se a função `show()` está definida no arquivo `sobreoprojeto.py`.")
+
+elif menu == "Sobre o Negócio":
+     try:
+        sobreonegocio.show()
+     except AttributeError:
+        st.error("Erro ao carregar a página 'Sobre o Negócio'. Verifique se a função `show()` está definida no arquivo `sobreonegocio.py`.")
+   
+elif menu == "Home":
+    try:
+        home.show()
+    except AttributeError:
+        st.error("Erro ao carregar a página 'Home'. Verifique se a função `show()` está definida no arquivo `home.py`.")
+
+
+
+
+

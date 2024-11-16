86c4fe45409e7c74d742a3646137c9cd715b6764
Gustavo Oliveira <gutti.bp@gmail.com>
2024-11-16 16:36:53 -0300

    Update mvpfinal.py

--- scripts/mvpfinal.py
+++ scripts/mvpfinal.py
@@ -2,15 +2,14 @@ import streamlit as st
 import pandas as pd
 ##import plotly.graph_objects as go  # Usando plotly.graph_objects
 
-def show():
     
-    left, cent, right = st.columns(3)
-    with right:
-        st.image('imagens/fiap.png')  # Certifique-se que o caminho para a imagem está correto
+left, cent, right = st.columns(3)
+with right:
+    st.image('imagens/fiap.png')  # Certifique-se que o caminho para a imagem está correto
    
     # Leitura dos dados
-    dados = pd.read_csv("dataset/Europe_Brent_Spot_Price_FOB.csv")
-    forecast = pd.read_csv("dataset/xgboost_results.csv")
+ dados = pd.read_csv("dataset/Europe_Brent_Spot_Price_FOB.csv")
+ forecast = pd.read_csv("dataset/xgboost_results.csv")
     
     # Tipando coluna de data e filtrando dados
     dados['Date'] = pd.to_datetime(dados['Date'], format='%Y-%m-%d', errors='coerce')

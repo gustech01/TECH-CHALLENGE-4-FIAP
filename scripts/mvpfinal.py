2e84ad1949d8b7a82b2ef62b3cdc2ebea3a16ecf
Gustavo Oliveira <gutti.bp@gmail.com>
2024-11-16 18:17:36 -0300

    Update mvpfinal.py

--- scripts/mvpfinal.py
+++ scripts/mvpfinal.py
@@ -12,8 +12,8 @@ dados = pd.read_csv("dataset/Europe_Brent_Spot_Price_FOB.csv")
 forecast = pd.read_csv("dataset/xgboost_results.csv")
 
 # Exibindo as primeiras linhas para verificar os dados
-st.write("### Visualização do Dataset Histórico")
-st.write(dados.head())  # Inspecione os nomes das colunas aqui
+#st.write("### Visualização do Dataset Histórico")
+#st.write(dados.head())  # Inspecione os nomes das colunas aqui
 
 # Tipando coluna de data e filtrando dados
 dados['Date'] = pd.to_datetime(dados['Date'])
@@ -29,8 +29,8 @@ else:
 forecast['Date'] = pd.to_datetime(forecast['Date'], format='%Y-%m-%d', errors='coerce')
 
 # Verificando colunas do dataset de previsões
-st.write("### Visualização do Dataset de Previsões")
-st.write(forecast.head())  # Inspecione os nomes das colunas aqui
+#st.write("### Visualização do Dataset de Previsões")
+#st.write(forecast.head())  # Inspecione os nomes das colunas aqui
  
 # Gráfico 1: Histórico do petróleo
 if 'Value' in dados.columns:

778bd5427240526c63954bf22f1bc4e6163bb81f
Gustavo Oliveira <gutti.bp@gmail.com>
2024-11-20 22:05:02 -0300

    Update appmain.py

--- scripts/appmain.py
+++ scripts/appmain.py
@@ -1,30 +1,40 @@
 import streamlit as st
-# Inicialize variáveis no session_state
+
+# Inicialize o estado da página apenas uma vez
 if "current_page" not in st.session_state:
     st.session_state.current_page = "Home"
 
 # Menu lateral
 st.sidebar.title("Menu")
-menu = st.sidebar.radio("Selecione uma página:", ["Home", "Sobre o Negócio", "Sobre o Projeto","MVP"])
+menu = st.sidebar.radio("Selecione uma página:", ["Home", "Sobre o Negócio", "Sobre o Projeto", "MVP"])
 
-# Atualize o estado da página atual
-st.session_state.current_page = menu
+# Atualize o estado da página
+if st.session_state.current_page != menu:
+    st.session_state.current_page = menu
 
-# Exibição das páginas
-if st.session_state.current_page == "Home":
+# Funções para exibir cada página
+def show_home():
     import home
     home.show()
 
-elif st.session_state.current_page == "Sobre o Negócio":
+def show_sobre_o_negocio():
     import sobreonegocio
     sobreonegocio.show()
 
-elif st.session_state.current_page == "Sobre o Projeto":
+def show_sobre_o_projeto():
     import sobreoprojeto
     sobreoprojeto.show()
 
-elif st.session_state.current_page == "MVP":
+def show_mvp():
     import mvpfinal
     mvpfinal.show()
 
-
+# Navegação entre as páginas
+if st.session_state.current_page == "Home":
+    show_home()
+elif st.session_state.current_page == "Sobre o Negócio":
+    show_sobre_o_negocio()
+elif st.session_state.current_page == "Sobre o Projeto":
+    show_sobre_o_projeto()
+elif st.session_state.current_page == "MVP":
+    show_mvp()

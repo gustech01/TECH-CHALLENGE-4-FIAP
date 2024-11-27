03a578e9cabf4fc5157ac31597e5c2cd624d7f2e
Gustavo Oliveira <gutti.bp@gmail.com>
2024-11-26 22:21:16 -0300

    Update mvpfinal.py

--- scripts/mvpfinal.py
+++ scripts/mvpfinal.py
@@ -111,7 +111,7 @@ def show():
             
             # Fórmula centralizada
              # Logo FIAP
-            left, cent, right = st.columns(2)
+            cent = st.columns(1)
             with right:
                 imagem_2 = carregar_imagem('imagens/formula_black_background.png')
                 if imagem_2:
